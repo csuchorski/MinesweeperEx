2ed4a9cf49640a7b173f092ca8b243775ab0f956
csuchorski <cezary.suchorski@wp.pl>
2022-09-18 14:55:11 +0200

    implemented Minesweeper.GameLogic.start_game/1 that starts a GameServer with generated game id and difficulty parameters

--- lib/minesweeper/game_logic.ex
+++ lib/minesweeper/game_logic.ex
@@ -6,14 +6,37 @@ defmodule Minesweeper.GameLogic do
     expert: {30, 16, 99}
   }
 
-  def start_game(params) do
+  def start_game(difficulty) do
     # Entry point of the front end, should start the Game GenServer that will setup,
     # Should return tuple {status, game_id}
+
+    difficulty = String.to_existing_atom(difficulty)
+    diff_tuple = Map.fetch!(@difficulty_table, difficulty)
+
+    game_params = %{
+      game_id: generate_id(),
+      width: diff_tuple |> elem(0),
+      height: diff_tuple |> elem(1),
+      mines_count: diff_tuple |> elem(2)
+    }
+
+    game =
+      DynamicSupervisor.start_child(
+        Minesweeper.DynamicSupervisor,
+        {Minesweeper.GameServer, game_params}
+      )
+
+    with {:ok, _pid} <- game do
+      {:ok, game_params.game_id}
+    else
+      {:error, message} -> {:error, message}
+    end
   end
 
-  def setup_board(params) do
+  def setup_board(difficulty) do
   end
 
+  @spec generate_id :: binary
   def generate_id() do
     :crypto.strong_rand_bytes(6) |> Base.url_encode64(padding: false)
   end
