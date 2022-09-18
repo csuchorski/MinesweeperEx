74a1462a187f01193457b67126fb47f347267f07
csuchorski <cezary.suchorski@wp.pl>
2022-09-18 14:09:40 +0200

    added generate_id/0 function to create url-safe strings for game identification

--- lib/minesweeper/game_logic.ex
+++ lib/minesweeper/game_logic.ex
@@ -1,10 +1,20 @@
 defmodule Minesweeper.GameLogic do
+  # {width, height, num_of_mines}
+  @difficulty_table %{
+    beginner: {10, 10, 10},
+    intermediate: {13, 15, 40},
+    expert: {30, 16, 99}
+  }
+
   def start_game(params) do
-    # Entry point of the front end, basically should start the Game GenServer that will setup,
+    # Entry point of the front end, should start the Game GenServer that will setup,
     # Should return tuple {status, game_id}
   end
 
   def setup_board(params) do
-    # return Enum of cells
+  end
+
+  def generate_id() do
+    :crypto.strong_rand_bytes(6) |> Base.url_encode64(padding: false)
   end
 end
