6b86ad71c3d315ddee7d9d90a6f20ba46164bc72
csuchorski <cezary.suchorski@wp.pl>
2022-09-19 14:30:15 +0200

    Implemented init callback to GameServer

--- lib/minesweeper/game_server.ex
+++ lib/minesweeper/game_server.ex
@@ -3,4 +3,22 @@ defmodule Minesweeper.GameServer do
     This module represents a GenServer that handles a minesweeper game instance.
   """
   use GenServer, restart: :temporary
+
+  def init(game_params) do
+    state =
+      Map.merge(game_params, %{
+        square_supervisor: nil,
+        flag_count: 0,
+        squares_revealed_count: 0,
+        mine_positions: nil
+      })
+
+    {:ok, state, {:continue, :start_game}}
+  end
+
+  def handle_call() do
+  end
+
+  def handle_cast() do
+  end
 end
