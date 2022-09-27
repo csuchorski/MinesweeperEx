ad67ae7f6c8acddcb825977a9955d817770f4b91
csuchorski <cezary.suchorski@wp.pl>
2022-09-27 11:10:19 +0200

    Started implementing handle_continue in the Game Server

--- lib/minesweeper/game_server.ex
+++ lib/minesweeper/game_server.ex
@@ -9,11 +9,19 @@ defmodule Minesweeper.GameServer do
       Map.merge(game_params, %{
         square_supervisor: nil,
         flag_count: 0,
-        squares_revealed_count: 0,
-        mine_positions: nil
+        squares_revealed_count: 0
       })
 
     {:ok, state, {:continue, :start_game}}
+  end
+
+  # Runs right after init of game server, generates the field and starts square servers
+  def handle_continue(:start_game, state) do
+    field = Minesweeper.GameLogic.setup_board(state)
+
+    for square <- field do
+      GenServer.start(Minesweeper.SquareServer, square)
+    end
   end
 
   def handle_call() do
