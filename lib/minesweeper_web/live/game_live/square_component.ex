56876ffd59b480d73d32c1fcc44d15202a18ab60
Cezary Suchorski <cezary.suchorski@gmail.com>
2022-10-02 18:52:43 +0200

    Added show function to square live component

--- lib/minesweeper_web/live/game_live/square_component.ex
+++ lib/minesweeper_web/live/game_live/square_component.ex
@@ -1,3 +1,11 @@
 defmodule MinesweeperWeb.GameLive.SquareComponent do
   use MinesweeperWeb, :live_component
+
+  def show(%{revealed?: false, marked?: false}), do: ""
+
+  def show(%{revealed?: false, marked?: true}), do: "flag"
+
+  def show(%{revealed?: true, value: :mine}), do: "mine!"
+
+  def show(%{revealed?: true, value: n}), do: n
 end
