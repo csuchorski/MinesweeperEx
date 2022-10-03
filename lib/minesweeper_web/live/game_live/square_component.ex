9011fea2f2d94a800771df2f372962ee1b6b73d5
csuchorski <cezary.suchorski@wp.pl>
2022-10-03 22:41:33 +0200

    Added mount and update method to square live component

--- lib/minesweeper_web/live/game_live/square_component.ex
+++ lib/minesweeper_web/live/game_live/square_component.ex
@@ -1,6 +1,17 @@
 defmodule MinesweeperWeb.GameLive.SquareComponent do
   use MinesweeperWeb, :live_component
 
+  alias Minesweeper.SquareServer
+
+  def mount(socket) do
+    {:ok, socket}
+  end
+
+  def update(assigns, socket) do
+    properties = SquareServer.get({assigns.game_id, assigns.coords})
+    {:ok, assign(socket, Map.merge(assigns, properties))}
+  end
+
   def show(%{revealed?: false, marked?: false}), do: ""
 
   def show(%{revealed?: false, marked?: true}), do: "flag"
