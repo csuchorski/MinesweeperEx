a485f8b5576968f48046f179b4b81a7a321698ec
Cezary Suchorski <cezary.suchorski@gmail.com>
2022-10-06 20:33:07 +0200

    Fixed incrementing near bombs

--- lib/minesweeper/game_logic.ex
+++ lib/minesweeper/game_logic.ex
@@ -72,43 +72,14 @@ defmodule Minesweeper.GameLogic do
       for mod_x <- -1..1,
           mod_y <- -1..1,
           square_coords = {bomb_x + mod_x, bomb_y + mod_y},
-          is_map_key(full_field, square_coords) == true,
+          is_map_key(full_field, square_coords),
+          Map.get(full_field, square_coords).value != :mine,
           reduce: full_field do
         full_field ->
           Map.update!(full_field, square_coords, fn square_properties ->
             %{square_properties | value: square_properties.value + 1}
           end)
-
-          nil
       end
     end)
   end
-
-  # defp increment_near_bombs(mine_squares, normal_squares) do
-  #   # Gets coords of all squares touching bombs or bombs themselves
-  #   # Getting spaghetti code vibes from this, needs improvement
-  #   squares_to_increment =
-  #     for {{bomb_x, bomb_y}, _} <- mine_squares,
-  #         modifier_x <- -1..1,
-  #         modifier_y <- -1..1 do
-  #       if modifier_x == 0 && modifier_y == 0 do
-  #         nil
-  #       else
-  #         {bomb_x + modifier_x, bomb_y + modifier_y}
-  #       end
-  #     end
-  #     |> Enum.reject(fn element -> element == nil end)
-  #     |> Enum.filter(fn {x, y} ->
-  #       x > 0 && x < 4 && y > 0 && y < 4
-  #     end)
-
-  #   incremented_normal_squares =
-  #     Enum.reduce(squares_to_increment, normal_squares, fn coords, acc ->
-  #       Map.update!(acc, coords, fn properties ->
-  #         %{properties | value: properties.value + 1}
-  #       end)
-  #     end)
-
-  #   Map.merge(Enum.into(mine_squares, %{}), incremented_normal_squares)
-  # end
 end
