0f8758a99664002995c2436035cca6ec64db604b
csuchorski <cezary.suchorski@wp.pl>
2022-10-18 13:59:27 +0200

    Implemented hook for marking a square with flags

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,8 +26,19 @@ import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+let Hooks = {}
+Hooks.GameControl = {
+    mounted() {
+        this.el.addEventListener('contextmenu', e => {
+            this.pushEventTo(this.el, "mark", {})
+            event.preventDefault()
+        }
+        )
+    }
+}
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken } })
+let liveSocket = new LiveSocket("/live", Socket, { params: { _csrf_token: csrfToken }, hooks: Hooks })
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
